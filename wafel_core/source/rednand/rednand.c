f7a4729fcab3c4dc87c4f864d59940eb300f3a05
Jan Hofmeier <jan.hofmeier@gmail.com>
2024-11-19 00:01:32 +0100

    disable sysmount if invalid config

--- wafel_core/source/rednand/rednand.c
+++ wafel_core/source/rednand/rednand.c
@@ -277,12 +277,12 @@ void rednand_init(rednand_config* rednand_conf, size_t config_size){
 
     if(mount_sys_mlc && redslc_size_sectors) {
         debug_printf("Mounting sysMLC as USB isn't allowed with SLC redirection\n");
-        return -1;
+        mount_sys_mlc = false;
     }
 
     if(mount_sys_mlc && partition_size && !disable_scfm){
         debug_printf("Mounting sysMLC on redMLC enabled is only possible with redNAND SCFM and SLC redirection disabled\n");
-        return -1;
+        mount_sys_mlc = false;
     }
 
     if(disable_scfm && !mount_sys_mlc){
