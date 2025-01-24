d1d7427d902b031afd5bd279dc60cfd01bf05471
Jan Hofmeier <jan.hofmeier@gmail.com>
2025-01-24 23:17:15 +0100

    LINKABLE boyer_more

--- wafel_core/include/wafel/bm.h
+++ wafel_core/include/wafel/bm.h
@@ -1,8 +1,10 @@
 #ifndef BM_H
 #define BM_H
 
+#include "types.h"
+
 // highly optimized data search, requires (256)+(4*patlen) stack 
 // returns NULL if pat not found
-void* boyer_moore_search(void *string, int stringlen, void *pat, int patlen);
+LINKABLE void* boyer_moore_search(void *string, int stringlen, void *pat, int patlen);
 
 #endif /* BM_H */
